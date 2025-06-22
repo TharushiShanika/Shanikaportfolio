69c9c297b55226955e37afe04d3bedd1f5ef1592
IT21171406 <99183050+IT21171406@users.noreply.github.com>
2025-06-22 17:44:07 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import './index.css';
-import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom"; // Add this import
+
+render(
+  <BrowserRouter basename="/Shanikaportfolio">
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
